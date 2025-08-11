f825d8dabbcc207035df5e119baa79677247df4b
Jiayao Yu <yjybxx2@yahoo.com>
2025-08-11 17:45:18 +0200

    Added ChatManager and NoticeBoard prefabs.

---
+++ Assets/Scripts/ChatManager.cs
@@ -0,0 +1,28 @@
+using System;
+using UnityEngine;
+using Unity.Netcode;
+
+public class ChatManager : NetworkBehaviour
+{
+    [Rpc(SendTo.Server)]
+    public void SendMessageServerRpc(string message,
+        ulong senderId)
+    {
+        // Server receives and broadcasts to all
+        ReceiveMessageClientRpc(message, senderId);
+    }
+    
+    [Rpc(SendTo.AllClients)]
+    public void ReceiveMessageClientRpc(string message,
+        ulong senderId)
+    {
+        // All clients display the message
+        string playerName = GetPlayerName(senderId);
+        DisplayMessage($"{playerName}: {message}");
+    }
+    
+    public void SendChatMessage(string message)
+    {
+        SendMessageServerRpc(message, NetworkManager.LocalClientId);
+    }
+}
